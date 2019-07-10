aeed1d1d8cc77404578e0e02aca9d18b43c4271e
laptop <nolic0321@gmail.com>
2019-07-09 19:06:19 -0500

    Started working on a growing Honeycomb.  Have final spritsheet ready.

---
+++ Assets/Hive.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.PandarooStudios.BeeSim
+{
+    public class Hive : MonoBehaviour
+    {
+        #region Private Variables
+
+        [SerializeField]
+        private Vector2 diagOffset;
+
+        private List<GameObject> endCombs;
+
+        [SerializeField]
+        private Vector2 offset;
+
+        #endregion Private Variables
+
+        #region Public Variables
+
+        public GameObject honeycombPrefab;
+
+        #endregion Public Variables
+
+        #region Monobehaviour Callbacks
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            endCombs = new List<GameObject>();
+            if (honeycombPrefab != null)
+            {
+                // Begin honecomb creation coroutine
+            }
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        }
+
+        #endregion Monobehaviour Callbacks
+
+        #region Coroutines
+
+        private IEnumerator CreateComb()
+        {
+            while (true)
+            {
+                // Create New honeycomb
+                // Wait
+            }
+        }
+
+        #endregion Coroutines
+    }
+}
---
+++ Assets/Honeycomb.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.PandarooStudios.BeeSim
+{
+    public class Honeycomb : MonoBehaviour
+    {
+        #region Private Variables
+
+        [SerializeField]
+        private bool endComb = true;
+
+        #endregion Private Variables
+
+        #region Properties
+
+        public bool IsEndComb
+        {
+            get
+            {
+                return endComb;
+            }
+        }
+
+        #endregion Properties
+
+        #region Private Methods
+
+        private bool EndCombCheck()
+        {
+            return true;
+        }
+
+        #endregion Private Methods
+    }
+}
